ff30493664665decd0a080c276472628897a0cbc
cactus_1549 <50070865+saikumar0512@users.noreply.github.com>
2020-10-28 14:34:52 +0530

    Create main.py
    main file

---
+++ main.py
@@ -0,0 +1,29 @@
+from sklearn import model_selection 
+from sklearn.ensemble import BaggingClassifier 
+from sklearn.tree import DecisionTreeClassifier 
+import pandas as pd 
+  
+url = "/home/debomit/Downloads/wine_data.xlsx"
+dataframe = pd.read_excel(url) 
+arr = dataframe.values 
+X = arr[:, 1:14] 
+Y = arr[:, 0] 
+  
+seed = 8
+kfold = model_selection.KFold(n_splits = 3, 
+                       random_state = seed) 
+  
+ 
+base_cls = DecisionTreeClassifier() 
+  
+ 
+num_trees = 500
+  
+
+model = BaggingClassifier(base_estimator = base_cls, 
+                          n_estimators = num_trees, 
+                          random_state = seed) 
+  
+results = model_selection.cross_val_score(model, X, Y, cv = kfold) 
+print("accuracy :") 
+print(results.mean())
